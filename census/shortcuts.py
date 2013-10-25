682a2948f0ee80faa002335349901af6db142366
Kenji Lefèvre <kenji.lefevre@math.cnrs.fr>
2013-10-25 10:54:25 +0200

    - Extract some constants out of load_datas - fit Transformer in three stages instead of two due to changes of Transformer 	modified:   census/shortcuts.py

--- census/shortcuts.py
+++ census/shortcuts.py
@@ -2,30 +2,32 @@
 from census.loader import CensusLoader
 from census.meta import cols
 
+TARGET = 'TARGET'
+TARGET_VALUE_1 = ' 50000+.'
+# continuous numerical columns
+NUM_COLS_STANDARD = ['AGE', 'INSTANCE_WEIGHT']
+NUM_COLS_WITH_ZERO_TREATMENT = \
+    ['WWORKY', 'WPERH', 'CAP_GAINS', 'CAP_LOSSES', 'STOCK_DIV']
+NUM_COLS = NUM_COLS_STANDARD + NUM_COLS_WITH_ZERO_TREATMENT
+# categorical cols
+CAT_COLS = [col for col in cols if col not in NUM_COLS + [TARGET]]
 
+
+# for linear regression
 # return data X, y, X_test, y_test, X_cols, X_cols_interpret
-def load_datas(dir='data', nrows=None, remove_cols=[]):
+def load_datas(dir='data', nrows=None, remove_cols=[], dummify=False):
     print "Loading data from disk..."
     learnds = CensusLoader(dir+'/census_income_learn.csv', cols=cols)
     learndf = learnds.get_data(nrows=nrows)
     testds = CensusLoader(dir+'/census_income_test.csv', cols=cols)
     testdf = testds.get_data(nrows=nrows)
 
-    target = ['TARGET']
-    # continuous numerical columns
-    num_cols_standard = ['AGE', 'INSTANCE_WEIGHT']
-    num_cols_with_zero_treatment = \
-        ['WWORKY', 'WPERH', 'CAP_GAINS', 'CAP_LOSSES', 'STOCK_DIV']
-    num_cols = num_cols_standard + num_cols_with_zero_treatment
-    # categorical cols
-    cat_cols = [col for col in cols if col not in num_cols + target]
-
     # TODO could be done in a better way
     # using the read_csv option index_col in census.Loader
     if remove_cols:
         print("Remove columns %r..." % remove_cols)
-        cat_cols = [col for col in cat_cols if col not in remove_cols]
-        num_cols = [col for col in num_cols if col not in remove_cols]
+        cat_cols = [col for col in CAT_COLS if col not in remove_cols]
+        # num_cols = [col for col in NUM_COLS if col not in remove_cols]
         learndf = learndf.drop(remove_cols, axis=1)
         testdf = testdf.drop(remove_cols, axis=1)
 
@@ -35,17 +37,18 @@ def load_datas(dir='data', nrows=None, remove_cols=[]):
     from census.transformer import Transformer
     ts = Transformer()
     # create dummy variables from categorical variables
-    ts.fit_dummification(learndf, cat_cols=cat_cols)
+    ts.fit_mapping(learndf, cat_cols=cat_cols)
+    ts.fit_dummification(learndf)
     # create a booleans for col with many zeros
-    ts.fit_vbools(num_cols_with_zero_treatment)
+    ts.fit_vbools(NUM_COLS_WITH_ZERO_TREATMENT)
 
     # perform transformation
-    n_learndf = ts.transform(learndf.drop(['TARGET'], axis=1))
-    n_testdf = ts.transform(testdf.drop(['TARGET'], axis=1))
+    n_learndf = ts.transform(learndf.drop([TARGET], axis=1))
+    n_testdf = ts.transform(testdf.drop([TARGET], axis=1))
 
     # get target
-    n_learn_target = (learndf['TARGET'] == ' 50000+.').astype(float)
-    n_test_target = (testdf['TARGET'] == ' 50000+.').astype(float)
+    n_learn_target = (learndf[TARGET] == TARGET_VALUE_1).astype(float)
+    n_test_target = (testdf[TARGET] == TARGET_VALUE_1).astype(float)
 
     # get human understandable col names of transformed df
     cols_interprets = [
